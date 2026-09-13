d3612c86de580e922994b1a63d6f7d37af5e245e
Matt Setaro <matthewsetaro@proton.me>
2026-09-13 19:07:31 -0400

    qcommon: C++ compatibility for cmd

--- code/qcommon/cmd.c
+++ code/qcommon/cmd.c
@@ -386,7 +386,7 @@ static void Cmd_Exec_f( void ) {
 	} f;
 	char filename[MAX_QPATH];
 
-	quiet = !Q_stricmp(Cmd_Argv(0), "execq");
+	quiet = (qboolean)( !Q_stricmp(Cmd_Argv(0), "execq") );
 
 	if (Cmd_Argc () != 2) {
 		Com_Printf ("exec%s <filename> : execute a script file%s\n",
@@ -772,7 +772,7 @@ void Cmd_AddCommand( const char *cmd_name, xcommand_t function ) {
 	}
 
 	// use a small malloc to avoid zone fragmentation
-	cmd = S_Malloc( sizeof( *cmd ) );
+	cmd = (cmd_function_t *)S_Malloc( sizeof( *cmd ) );
 	cmd->name = CopyString( cmd_name );
 	cmd->function = function;
 	cmd->complete = NULL;
