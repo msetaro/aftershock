c75d15c73c18d71589ec5c12c70b1ec1e4d400f3
Matt Setaro <matthewsetaro@proton.me>
2026-09-13 20:02:49 -0400

    renderer: C++ compatibility for tr_scene

--- code/renderer/tr_scene.c
+++ code/renderer/tr_scene.c
@@ -104,7 +104,7 @@ void R_AddPolygonSurfaces( void ) {
 
 	for ( i = 0, poly = tr.refdef.polys; i < tr.refdef.numPolys ; i++, poly++ ) {
 		sh = R_GetShaderByHandle( poly->hShader );
-		R_AddDrawSurf( ( void * )poly, sh, poly->fogIndex, 0 );
+		R_AddDrawSurf( (surfaceType_t *)( void * )poly, sh, poly->fogIndex, 0 );
 	}
 }
 
