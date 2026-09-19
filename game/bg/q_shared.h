229ccb6446afda822f96677e622c0502e5eae058
Matt Setaro <matthewsetaro@proton.me>
2026-09-19 16:12:13 -0400

    build: enforce MSVC type redefinition warnings

--- game/bg/q_shared.h
+++ game/bg/q_shared.h
@@ -39,7 +39,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #pragma warning(disable : 4136)
 //#pragma warning(disable : 4201)
 //#pragma warning(disable : 4214)
-#pragma warning(disable : 4142)		// benign redefinition
 //#pragma warning(disable : 4305)		// truncation from const double to float
 //#pragma warning(disable : 4310)		// cast truncates constant value
 //#pragma warning(disable:  4505) 	// unreferenced local function has been removed
