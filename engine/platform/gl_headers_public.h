231355cc26cf07bf6b1d529619e6902dd89047f2
Matt Setaro <matthewsetaro@proton.me>
2026-09-19 17:29:34 -0400

    build: enforce MSVC C4214 warnings

--- engine/platform/gl_headers_public.h
+++ engine/platform/gl_headers_public.h
@@ -26,9 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #if defined( _WIN32 )
 #if _MSC_VER
-#pragma warning (disable: 4214)
 #pragma warning (disable: 4514)
-#pragma warning (disable: 4214)
 #endif
 #include <windows.h>
 #include <GL/gl.h>
