76876e91d3dd7663e92e7897890467fed508a134
Matt Setaro <matthewsetaro@proton.me>
2026-09-21 19:23:46 -0400

    sound: specify mixed event samples and voice limits

--- tests/probes/audio_events.cpp
+++ tests/probes/audio_events.cpp
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
 
 int main( int argc, char **argv ) {
 	assert( argc == 2 );
@@ -23,4 +24,47 @@ int main( int argc, char **argv ) {
 	assert( S_EventLayerGain( event.layers[2], 255 ) == 0.0f );
 	assert( S_EventLayerGain( event.layers[2], 256 ) == .75f );
 	assert( S_EventLayerGain( event.layers[2], 4096 ) == 0.0f );
+	static sEventMixer_t mixer;
+	static int16_t pcm[480] = {};
+	for ( auto &sample : pcm )
+		sample = 1000;
+	sEventPCM_t samples[4] = {};
+	for ( int i = 0; i < 3; ++i )
+		samples[i] = { pcm, 480, 48000, 1 };
+	sSpatialInput_t spatial = {};
+	spatial.right[1] = -1;
+	spatial.offset[1] = -40;
+	spatial.speedOfSound = 13504;
+	float output[256][2] = {};
+	assert( S_StartEventVoice( &mixer, &event, samples, spatial, false, 48000, .0875f ) >= 0 );
+	S_MixEvents( &mixer, output, 256, 48000 );
+	assert( output[100][0] == 0 && std::fabs( output[100][1] - 1500 ) < .01f );
+	assert( mixer.active == 1 );
+	S_MixEvents( &mixer, output, 256, 48000 );
+	assert( mixer.active == 0 );
+	mixer = {};
+	memset( output, 0, sizeof( output ) );
+	spatial.offset[1] = -2048;
+	assert( S_StartEventVoice( &mixer, &event, samples, spatial, false, 48000, .0875f ) >= 0 );
+	S_MixEvents( &mixer, output, 256, 48000 );
+	assert( std::fabs( output[100][1] - 750 * (80.0f / 2048.0f) ) < .01f );
+	mixer = {};
+	event.voiceLimit = 2;
+	spatial.offset[1] = -40;
+	for ( int i = 0; i < 10; ++i )
+		assert( S_StartEventVoice( &mixer, &event, samples, spatial, false, 48000, .0875f ) >= 0 );
+	assert( mixer.active == 2 && mixer.stolen == 8 );
+	auto low = event;
+	low.priority = event.priority - 1;
+	assert( S_StartEventVoice( &mixer, &low, samples, spatial, false, 48000, .0875f ) == -1 );
+	assert( mixer.active == 2 );
+	mixer = {};
+	assert( S_StartEventVoice( &mixer, &event, samples, spatial, true, 48000, .0875f ) >= 0 );
+	for ( int i = 0; i < 20; ++i ) {
+		memset( output, 0, sizeof( output ) );
+		S_MixEvents( &mixer, output, 256, 48000 );
+		for ( const auto &frame : output )
+			assert( std::isfinite( frame[0] ) && std::isfinite( frame[1] ) );
+	}
+	assert( mixer.active == 0 );
 }
